61952f6d89396b159cccac6cc397770a4d0b447b
Michael Henderson <mike.w.henderson.88@gmail.com>
2015-06-16 01:05:08 -0400

    Adding GET functionality for director, by id. Currently, data is all coming from local database. No data is being pulled from Livestream yet.

--- controllers/directors.js
+++ controllers/directors.js
@@ -1,11 +1,13 @@
 exports.findAll = function(req, res) {
 	var model = require('../models/directorsModel.js');
-	model.getDirectors(req, res);
+	model.getDirectors(res);
 	//res.send("Retrieve all directors");
 };
 
 exports.findById = function(req, res) {
-	res.send("Retrieve director id: " + req.params.id);
+	var model = require('../models/directorsModel.js');
+	model.getDirectorById(req.params.id, res);
+	//res.send("Retrieve director id: " + req.params.id);
 };
 
 exports.add = function(req, res) {
--- models/directorsModel.js
+++ models/directorsModel.js
@@ -1,8 +1,23 @@
-exports.getDirectors = function(req, res) {
+exports.getDirectors = function(res) {
 	var connPool = require('../database/db.js');
 
 	connPool.getConnection(function(err, connection) {
 		connection.query("select * from user", function(err, rows) {
+			if(err) {
+				throw err;
+			} else {
+				res.json(rows);
+			}
+		});
+		connection.release();
+	});
+};
+
+exports.getDirectorById = function(id, res) {
+	var connPool = require('../database/db.js');
+
+	connPool.getConnection(function(err, connection) {
+		connection.query("select * from user where user_id = ?", id, function(err, rows){
 			if(err) {
 				throw err;
 			} else {
