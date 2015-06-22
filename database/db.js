1b4457943806e0209cd52379cf0c96767b5537ee
Michael Henderson <mh747@aol.com>
2015-06-22 17:34:33 -0400

    Removing everything, need to start over

--- database/db.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-	host	: "localhost",
-	user	: "test",
-	password: "test",
-	database: "mikestream"
-});
-
-module.exports = pool;
--- routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-	var directors = require('./controllers/directors');
-	app.get('/mikestream/directors', directors.findAll);
-	app.get('/mikestream/directors/:id', directors.findByLsId);
-	app.post('/mikestream/directors', directors.add);
-	app.put('/mikestream/directors/:id', directors.update);
-}
