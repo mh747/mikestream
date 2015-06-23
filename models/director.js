22f53b5eb12478a63fb481ca8cfef789cd7ce3af
Michael Henderson <mh747@aol.com>
2015-06-23 15:24:42 -0400

    Adding post functionality for /directors/ route.

--- models/director.js
+++ models/director.js
@@ -18,12 +18,12 @@ Director.findAll = function(callback) {
 					return callback(err, null);
 				}
 				var record = {
-					ls_id: reply[0],
+					livestream_id: reply[0],
 					full_name: '',
 					dob: '',
 					favorite_camera: reply[1],
 					favorite_movies: reply[2]
-				}
+				};
 				var request = require('request');
 				request({
 					uri: "https://api.new.livestream.com/accounts/" + reply[0],
@@ -52,12 +52,12 @@ Director.findById = function(id, callback) {
 	
 	//Setting up return object
 	var data = {
-		ls_id: id,
+		livestream_id: id,
 		full_name: '',
 		dob: '',
 		favorite_camera: '',
 		favorite_movies: '' 
-	}
+	};
 
 	//Now finding local user id for given livestream id
 	client.hget('users', id, function(err, reply) {
@@ -103,5 +103,93 @@ Director.findById = function(id, callback) {
 	});
 }
 
+Director.add = function(director, callback) {
+	var client = require('../data');
+	//First, make sure director isn't already in system
+	client.hexists('users', director.livestream_id, function(err, reply) {
+		if(err) {
+			return callback(err, null);
+		}
+		if(reply === 1) {
+			var err = new Error("Livestream ID is already registered as an account");
+			return callback(err, null);
+		}
+
+		//Setting up return object
+		var data = {
+			livestream_id: director.livestream_id,
+			full_name: '',
+			dob: '',
+			favorite_camera: director.favorite_camera,
+			favorite_movies: director.favorite_movies
+		};
+
+		var dataSources = 2;
+		var completedReqs = 0;
+
+		//Database operations and http call to livestream run async
+		client.get('next_user_id', function(err, next_user_id) {
+			if(err) {
+				return callback(err, null);
+			}
+			console.log('entering user ' + next_user_id);
+			console.log(director.livestream_id);
+
+			//Lookup user to make sure its valid Livestream ID
+			var request = require('request');
+			request({
+				uri: "https://api.new.livestream.com/accounts/" + director.livestream_id,
+				method: "GET",
+				timeout: 10000,
+				followRedirect: true,
+				maxRedirects: 10
+			}, function(err, response, body) {
+				if(err) {
+					return callback(err, null);
+				}
+
+				if(response.statusCode !== 200) {
+					err = new Error("User ID " + director.livestream_id + " is not a valid user.");
+					return callback(err, null);
+				}
+
+				//Adding to db and processing http response async
+				client.hmset('user:' + next_user_id, 'ls_id', director.livestream_id, 
+				'favorite_camera', director.favorite_camera, 'favorite_movies', director.favorite_movies,
+				function(err, reply) {
+					if(err) {
+						return callback(err, null);
+					}
+
+					client.hset('users', director.livestream_id, next_user_id, function(err, reply) {
+						if(err) {
+							return callback(err, null);
+						}
+
+						client.incr('next_user_id', function(err, reply) {
+							if(err) {
+								return callback(err, null);
+							}
+
+							completedReqs++;
+							if(completedReqs === dataSources) {
+								return callback(null, data);
+							}
+						});
+					});
+				});
+
+				var parsed = JSON.parse(body);
+				data.full_name = parsed.full_name;
+				data.dob = parsed.dob;
+				completedReqs++;
+				if(completedReqs === dataSources) {
+					return callback(null, data);
+				}
+			});		
+
+		});
+	});
+}
 
 module.exports = Director;
--- routes/directors.js
+++ routes/directors.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.route('/directors').get(function(req, res) {
 	Director.findAll(function(err, directors) {
 		if(err) {
-			return res.send(err);
+			return res.send(err.message);
 		}
 
 		res.json(directors);
@@ -15,10 +15,31 @@ router.route('/directors').get(function(req, res) {
 router.route('/directors/:id').get(function(req, res) {
 	Director.findById(req.params.id, function(err, director) {
 		if(err) {
-			return res.send(err);
+			return res.send(err.message);
 		}
 
 		res.json(director);
+	});
+});
+
+router.route('/directors').post(function(req, res) {
+	if(!req.body.livestream_id) {
+		var err = new Error("Livestream ID is required to create a new account");
+		return res.send(err.message);
+	}
+
+	//Set up new director object and send to Director model
+	var director = {
+		livestream_id: req.body.livestream_id,
+		favorite_camera: (req.body.favorite_camera) ? req.body.favorite_camera : '',
+		favorite_movies: (req.body.favorite_movies) ? req.body.favorite_movies : ''
+	};
+	Director.add(director, function(err, newRecord) {
+		if(err) {
+			return res.send(err.message);
+		}
+
+		res.json(newRecord);
 	});
 });
 
