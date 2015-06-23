141f433d8ea91092e674f0cf09a96cfc1724fa6f
Michael Henderson <mh747@aol.com>
2015-06-23 19:15:28 -0400

    Small changes, error handling, etc

--- models/director.js
+++ models/director.js
@@ -6,17 +6,30 @@ Director.findAll = function(callback) {
 	var data = [];
 
 	client.get('next_user_id', function(err, reply) {
-		var numRecords = reply - 1;
+		if(err) {
+			err.statusCode = 500;
+			return callback(err, null);
+		}
+
+		if(!reply) {
+			var err = new Error("There are no users in the system");
+			err.statusCode = 404;
+			return callback(err, null);
+		}
+
+		var numRecords = +reply;  //Force type int
 		var completedReqs = 0;
 		console.log('Got next-user-id: ' + numRecords);
 		var i;
 		
-		for(i=1; i<=numRecords; i++) {
+		for(i=0; i<numRecords; i++) {
 			console.log('i = ' + i);
-			client.hmget('user:' + i, 'ls_id', 'favorite_camera', 'favorite_movies', function(err, reply) {
+			client.hmget('user:' + i, 'livestream_id', 'favorite_camera', 'favorite_movies', function(err, reply) {
 				if(err) {
+					err.statusCode = 500;
 					return callback(err, null);
 				}
+				console.log('got data from db');
 				var record = {
 					livestream_id: reply[0],
 					full_name: '',
@@ -32,6 +45,11 @@ Director.findAll = function(callback) {
 					followRedirect: true,
 					maxRedirects: 10
 				}, function(err, response, body) {
+					if(err) {
+						err.statusCode = 500;
+						return callback(err, null);
+					}
+					console.log('got data from livestream');
 					var parsed = JSON.parse(body);
 					record.full_name = parsed.full_name;
 					record.dob = parsed.dob;
@@ -39,7 +57,7 @@ Director.findAll = function(callback) {
 					completedReqs++;
 
 					if(completedReqs === numRecords) 
-						return callback(err, data);
+						return callback(null, data);
 				});
 				
 			});
@@ -62,16 +80,19 @@ Director.findById = function(id, callback) {
 	//Now finding local user id for given livestream id
 	client.hget('users', id, function(err, reply) {
 		if(err) {
+			err.statusCode = 500;
 			return callback(err, null);
 		}
 		if(!reply) {
-			var error = new Error("User with id:" + id + " does not exist");
+			var err = new Error("User with id:" + id + " does not exist");
+			err.statusCode = 404;
 			return callback(err, null);
 		}
 		var dataSources = 2; // Represents number of sources for data (database, livestream api)
 		var completedReqs = 0; // Number of finished requests for data
 		client.hmget('user:' + reply, 'favorite_camera', 'favorite_movies', function(err, reply) {
 			if(err) {
+				err.statusCode = 500;
 				return callback(err, null);
 			}
 			data.favorite_camera = reply[0];
@@ -91,6 +112,10 @@ Director.findById = function(id, callback) {
 			followRedirect: true,
 			maxRedirects: 10
 		}, function(err, response, body) {
+			if(err) {
+				err.statusCode = 500;
+				return callback(err, null);
+			}
 			var parsed = JSON.parse(body);
 			data.full_name = parsed.full_name;
 			data.dob = parsed.dob;
@@ -108,10 +133,12 @@ Director.add = function(director, callback) {
 	//First, make sure director isn't already in system
 	client.hexists('users', director.livestream_id, function(err, reply) {
 		if(err) {
+			err.statusCode = 500;
 			return callback(err, null);
 		}
 		if(reply === 1) {
 			var err = new Error("Livestream ID is already registered as an account");
+			err.statusCode = 400;
 			return callback(err, null);
 		}
 
@@ -130,7 +157,11 @@ Director.add = function(director, callback) {
 		//Database operations and http call to livestream run async
 		client.get('next_user_id', function(err, next_user_id) {
 			if(err) {
+				err.statusCode = 500;
 				return callback(err, null);
+			}
+			if(!next_user_id) {
+				next_user_id = 0;
 			}
 			console.log('entering user ' + next_user_id);
 			console.log(director.livestream_id);
@@ -145,29 +176,34 @@ Director.add = function(director, callback) {
 				maxRedirects: 10
 			}, function(err, response, body) {
 				if(err) {
+					err.statusCode = 500;
 					return callback(err, null);
 				}
 
 				if(response.statusCode !== 200) {
 					err = new Error("User ID " + director.livestream_id + " is not a valid user.");
+					err.statusCode = 400;
 					return callback(err, null);
 				}
 
 				//Adding to db and processing http response async
-				client.hmset('user:' + next_user_id, 'ls_id', director.livestream_id, 
+				client.hmset('user:' + next_user_id, 'livestream_id', director.livestream_id, 
 				'favorite_camera', director.favorite_camera, 'favorite_movies', director.favorite_movies,
 				function(err, reply) {
 					if(err) {
+						err.statusCode = 500;
 						return callback(err, null);
 					}
 
 					client.hset('users', director.livestream_id, next_user_id, function(err, reply) {
 						if(err) {
+							err.statusCode = 500;
 							return callback(err, null);
 						}
 
 						client.incr('next_user_id', function(err, reply) {
 							if(err) {
+								err.statusCode = 500;
 								return callback(err, null);
 							}
 
@@ -190,6 +226,77 @@ Director.add = function(director, callback) {
 
 		});
 	});
+}
+
+Director.modify = function(id, fields, callback) {
+	var client = require('../data');
+	//Using given livestream id to lookup local user record
+
+	client.hget('users', id, function(err, user_id) {
+		if(err) {
+			err.statusCode = 500;
+			return callback(err, null);
+		}
+
+		//Updating local user fields, and sending http request to livestream for
+		//return value
+		var data = {
+			livestream_id: id,
+			full_name: '',
+			dob: '',
+			favorite_camera: '',
+			favorite_movies: ''
+		}
+
+		var dataSources = 2;
+		var completedReqs = 0;
+
+		//HTTP request
+		var request = require('request');
+		request({
+			uri: "https://api.new.livestream.com/accounts/" + id,
+			method: "GET",
+			timeout: 10000,
+			followRedirect: true,
+			maxRedirects: 10
+		}, function(err, response, body) {
+			if(err) {
+				err.statusCode = 500;
+				return callback(err, null);
+			}
+
+			var parsed = JSON.parse(body);
+			data.full_name = parsed.full_name;
+			data.dob = parsed.dob;
+			completedReqs++;
+			if(completedReqs === dataSources) {
+				return callback(null, data);
+			}
+		});
+
+		client.hmset('user:' + user_id, fields, function(err, reply) {
+			if(err) {
+				err.statusCode = 500;
+				return callback(err, null);
+			}
+
+			//Now set the return data to new value
+			client.hmget('user:' + user_id, 'favorite_camera', 'favorite_movies', function(err, reply) {
+				if(err) {
+					err.statusCode = 500;
+					return callback(err, null);
+				}
+				data.favorite_camera = reply[0];
+				data.favorite_movies = reply[1];
+				completedReqs++;
+				if(completedReqs === dataSources) {
+					return callback(null, data);
+				}
+			});
+		});
+		
+	});
+
 }
 
 module.exports = Director;
--- routes/directors.js
+++ routes/directors.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.route('/directors').get(function(req, res) {
 	Director.findAll(function(err, directors) {
 		if(err) {
+			res.status(err.statusCode);
 			return res.send(err.message);
 		}
 
@@ -15,6 +16,7 @@ router.route('/directors').get(function(req, res) {
 router.route('/directors/:id').get(function(req, res) {
 	Director.findById(req.params.id, function(err, director) {
 		if(err) {
+			res.status(err.statusCode);
 			return res.send(err.message);
 		}
 
@@ -25,6 +27,7 @@ router.route('/directors/:id').get(function(req, res) {
 router.route('/directors').post(function(req, res) {
 	if(!req.body.livestream_id) {
 		var err = new Error("Livestream ID is required to create a new account");
+		res.status(400);
 		return res.send(err.message);
 	}
 
@@ -36,10 +39,29 @@ router.route('/directors').post(function(req, res) {
 	};
 	Director.add(director, function(err, newRecord) {
 		if(err) {
+			res.status(err.statusCode);
 			return res.send(err.message);
 		}
 
 		res.json(newRecord);
+	});
+});
+
+router.route('/directors/:id').put(function(req, res) {
+	var director = [];
+	if(req.body.favorite_camera) {
+		director.favorite_camera = req.body.favorite_camera;
+	}
+	if(req.body.favorite_movies) {
+		director.favorite_movies = req.body.favorite_movies;
+	}
+	Director.modify(req.params.id, director, function(err, modRecord) {
+		if(err) {
+			res.status(err.statusCode);
+			return res.send(err.message);
+		}
+
+		res.json(modRecord);
 	});
 });
 
